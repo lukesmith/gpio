3eb4e89a49d2b33951a0d17dd5da6d63b7abba80
Raspberry Pi User <pi@raspberrypi.(none)>
2012-06-16 23:17:00 +0100

    needed to change the raspberrypi to mapping software pins

--- lib/gpio/devices/raspberry_pi.rb
+++ lib/gpio/devices/raspberry_pi.rb
@@ -5,7 +5,7 @@ module GPIO
 		VALIDATE_FILE = "/sys/class/gpio/gpiochip0/label"
 		VALIDATE_VALUE = "bcm2708_gpio"
 
-		MAPPING = :hardware
+		MAPPING = :software
 		HARDWARE_PINS = [3,5,7,8,10,11,12,13,15,16,17,19,21,22,23,24,26]
 		SOFTWARE_PINS = [0,1,4,14,15,17,18,21,22,23,24,10,9,25,11,8,7]
 		BASE_PATH = '/sys/class/gpio/'
